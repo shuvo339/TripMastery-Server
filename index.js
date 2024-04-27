354c7edb56c3417264e23b0f2c0b8f46d28caa59
shuvo339 <shubhroshafiullah@gmail.com>
2024-04-27 09:10:00 +0600

    delete method implemented

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v8mpgvp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,12 +29,19 @@ async function run() {
       const result = await tourSpotCollection.find().toArray();
       res.send(result); 
     })
-
     app.post('/tourspots', async(req,res)=>{
       const tourSpot = req.body;
       const result = await tourSpotCollection.insertOne(tourSpot);
       res.send(result); 
     })
+
+    app.delete('/tourspots/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await tourSpotCollection.deleteOne(query);
+      res.send(result); 
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
